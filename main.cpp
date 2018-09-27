c06cdbc142e600e4f7fbefe69a63d82088cc644a
JukerHD <chanleesing@gmail.com>
2018-09-26 17:09:58 -0700

    lab1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab1
+//
+//  Created by lee sing chan on 9/26/18.
+//  Copyright © 2018 Lee Sing Chan Yau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
